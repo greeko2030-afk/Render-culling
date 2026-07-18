ca0fbe828b66563b189e37aefb78c828cf1e0cd2
greeko2030-afk <greeko2030@gmail.com>
2026-07-18 16:03:42 +0600

    Update FrustumMath.java

--- src/main/java/com/mixin/util/FrustumMath.java
+++ src/main/java/com/mixin/util/FrustumMath.java
@@ -1,1 +1,20 @@
 
+package com.greeko.renderculling.util;
+
+import net.minecraft.client.render.Frustum;
+import net.minecraft.util.math.Box;
+
+public class FrustumMath {
+    private static Frustum currentFrustum;
+
+    public static void setFrustum(Frustum frustum) {
+        currentFrustum = frustum;
+    }
+
+    public static boolean isVisible(Box boundingBox) {
+        if (currentFrustum == null || boundingBox == null) {
+            return true; // Default to rendering if camera state is unknown to prevent invisible entities
+        }
+        return currentFrustum.isVisible(boundingBox);
+    }
+}
