1112ab62d677bf77b2a728d4143fbf72c4120bd6
greeko2030-afk <greeko2030@gmail.com>
2026-07-18 21:58:49 +0600

    Update BlockEntityMixin.java

--- src/main/java/com/mixin/BlockEntityMixin.java
+++ src/main/java/com/mixin/BlockEntityMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Box;
 import com.greeko.renderculling.util.FrustumMath;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +16,11 @@ public class BlockEntityMixin {
 
     @Inject(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", at = @At("HEAD"), cancellable = true)
     private void onRenderBlockEntity(BlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
-        if (!FrustumMath.isVisible(blockEntity.getRenderBoundingBox())) {
-            ci.cancel();
+        if (blockEntity.hasWorld()) {
+            Box box = new Box(blockEntity.getPos());
+            if (!FrustumMath.isVisible(box)) {
+                ci.cancel();
+            }
         }
     }
 }
