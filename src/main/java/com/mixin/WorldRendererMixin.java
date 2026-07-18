1b77800fe68f39ee47998ae91ec9b347166182bd
greeko2030-afk <greeko2030@gmail.com>
2026-07-18 16:04:35 +0600

    Update WorldRendererMixin.java

--- src/main/java/com/mixin/WorldRendererMixin.java
+++ src/main/java/com/mixin/WorldRendererMixin.java
@@ -1,1 +1,19 @@
+package com.greeko.renderculling.mixin;
+
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.WorldRenderer;
+import com.greeko.renderculling.util.FrustumMath;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+
+    @Inject(method = "setupFrustum", at = @At("TAIL"))
+    private void onSetupFrustum(Frustum frustum, CallbackInfo ci) {
+        FrustumMath.setFrustum(frustum);
+    }
+}
 
