9fe80aaca21fe08aa735824caac191cb3e1f756c
greeko2030-afk <greeko2030@gmail.com>
2026-07-18 16:02:48 +0600

    Implement RenderCullingClient with logging
    Added logging for client initialization and optimization.

--- src/main/java/com/greeko/renderculling/RenderCullingClient.java
+++ src/main/java/com/greeko/renderculling/RenderCullingClient.java
@@ -1,1 +1,16 @@
+package com.greeko.renderculling;
 
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RenderCullingClient implements ClientModInitializer {
+    public static final String MOD_ID = "renderculling";
+    public static final Logger LOGGER = LoggerFactory.getLogger("Greeko Render Culling");
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Greeko Render Culling initialized for Minecraft 1.21.x!");
+        LOGGER.info("Optimizing entities and block entities for maximum FPS...");
+    }
+}
