64d575d92c0ae1724363e623038d52d4d169e91b
greeko2030-afk <greeko2030@gmail.com>
2026-07-18 16:05:19 +0600

    Update EntityRenderMixin.java

--- src/main/java/com/mixin/EntityRenderMixin.java
+++ src/main/java/com/mixin/EntityRenderMixin.java
@@ -1,1 +1,22 @@
+package com.greeko.renderculling.mixin;
 
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import com.greeko.renderculling.util.FrustumMath;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderDispatcher.class)
+public class EntityRenderMixin {
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private <E extends Entity> void onRenderEntity(E entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        if (!FrustumMath.isVisible(entity.getBoundingBoxForCulling())) {
+            ci.cancel();
+        }
+    }
+}
