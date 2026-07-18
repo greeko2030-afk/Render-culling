32c412af912665792f1f9942fa6b6106759a0a2f
greeko2030-afk <greeko2030@gmail.com>
2026-07-18 16:06:12 +0600

    Update BlockEntityMixin.java

--- src/main/java/com/mixin/BlockEntityMixin.java
+++ src/main/java/com/mixin/BlockEntityMixin.java
@@ -1,1 +1,22 @@
+package com.greeko.renderculling.mixin;
 
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import com.greeko.renderculling.util.FrustumMath;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockEntityRenderDispatcher.class)
+public class BlockEntityMixin {
+
+    @Inject(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", at = @At("HEAD"), cancellable = true)
+    private <E extends BlockEntity> void onRenderBlockEntity(E blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+        if (!FrustumMath.isVisible(blockEntity.getRenderBoundingBox())) {
+            ci.cancel();
+        }
+    }
+}
